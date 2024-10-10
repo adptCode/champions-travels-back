ce7068b40f2b74e048bf24101cb90de55bee6135
Alessandro De Pietri Tonelli <adepietri.it@gmail.com>
2024-10-10 15:34:19 +0200

    local-ok

--- src/db.js
+++ src/db.js
@@ -9,8 +9,7 @@ const sequelize = new Sequelize(
   process.env.PASSWORD,
   {
     host: process.env.HOST_NAME,
-    dialect: 'mysql',
-    port: process.env.DB_PORT || 3306
+    dialect: 'mysql'
   }
 );
 
