415cd4c0c6e9f33d605597ebc144e4492d96dc77
Alessandro De Pietri Tonelli <adepietri.it@gmail.com>
2024-12-03 15:12:32 +0100

    fix bug middleware

--- src/middlewares/upload-event.js
+++ src/middlewares/upload-event.js
@@ -1,20 +1,11 @@
 import util from "util";
 import multer from "multer";
 import path from 'path';
-import sharp from "sharp";
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { storage } from "./firebaseConfig"; // Configuracion Firebase
-import dotenv from "dotenv";
+const maxSize = 2 * 1024 * 1024;
 
-dotenv.config();
-const isProduction = process.env.NODE_ENV === "production"; // Verifica l'ambiente
-
-const maxSize = 2 * 1024 * 1024; // 2MB massimo
-
-// Configurazione Multer
-const storageLocal = multer.diskStorage({
+const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./src/uploads-event/"); // Salva localmente
+    cb(null, "./src/uploads-event/");
   },
   filename: function (req, file, cb) {
     console.log("file.originalname: "+file.originalname);
@@ -23,29 +14,9 @@ const storageLocal = multer.diskStorage({
   },
 });
 
-const storageMemory = multer.memoryStorage(); // Salva in memoria per Firebase
-
-const uploadFile = multer({
-  storage: isProduction ? storageMemory : storageLocal, // Usa memoria in produzione, locale in sviluppo
+let uploadFile = multer({
+  storage: storage,
   limits: { fileSize: maxSize },
 }).single('photo')
 
 export const uploadEventFileMiddleware = util.promisify(uploadFile);
-
-// Funzione per il caricamento su Firebase (produzione)
-export const processAndUploadFile = async (file) => {
-  // Comprimi il file con Sharp
-  const compressedBuffer = await sharp(file.buffer)
-    .resize({ width: 800 }) // Ridimensiona
-    .jpeg({ quality: 80 }) // Comprimi
-    .toBuffer();
-
-  // Carica su Firebase
-  const fileName = `${file.fieldname}-${Date.now()}.jpeg`;
-  const storageRef = ref(storage, `uploads/${fileName}`); // Cartella nel bucket
-
-  const snapshot = await uploadBytes(storageRef, compressedBuffer); // Carica
-  const downloadURL = await getDownloadURL(snapshot.ref); // Ottieni URL
-
-  return downloadURL;
-};
--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -2,22 +2,52 @@
 import util from "util";
 import multer from "multer";
 import path from 'path';
+import sharp from "sharp";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { storage } from "../../firebaseConfig"
+import dotenv from "dotenv";
+
+dotenv.config();
+const isProduction = process.env.NODE_ENV === "production"; // Verifica l'ambiente
+
 const maxSize = 2 * 1024 * 1024;
 
-const storage = multer.diskStorage({
+// Configurazione Multer
+const storageLocal = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./src/uploads/");
+    cb(null, "./src/uploads/"); // Salva localmente
   },
   filename: function (req, file, cb) {
-    console.log("file.originalname: "+file.originalname);
-    cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
     );
   },
 });
 
-let uploadFile = multer({
-  storage: storage,
+const storageMemory = multer.memoryStorage(); // Salva in memoria per Firebase
+
+const uploadFile = multer({
+  storage: isProduction ? storageMemory : storageLocal, // Usa memoria in produzione, locale in sviluppo
   limits: { fileSize: maxSize },
-}).single('file')
+}).single("file");
 
 export const uploadFileMiddleware = util.promisify(uploadFile);
+
+// Funzione per il caricamento su Firebase (produzione)
+export const processAndUploadFile = async (file) => {
+  // Comprimi il file con Sharp
+  const compressedBuffer = await sharp(file.buffer)
+    .resize({ width: 800 }) // Ridimensiona
+    .jpeg({ quality: 80 }) // Comprimi
+    .toBuffer();
+
+  // Carica su Firebase
+  const fileName = `${file.fieldname}-${Date.now()}.jpeg`;
+  const storageRef = ref(storage, `uploads/${fileName}`); // Cartella nel bucket
+
+  const snapshot = await uploadBytes(storageRef, compressedBuffer); // Carica
+  const downloadURL = await getDownloadURL(snapshot.ref); // Ottieni URL
+
+  return downloadURL;
+};
